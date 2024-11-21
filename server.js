6078b37dce0b9273a0987d7c558f6db9239ed511
kamehamehaa0000 <gayush794@gmail.com>
2024-11-21 16:25:40 +0530

    added

--- server.js
+++ server.js
@@ -13,3 +13,22 @@ app.use('/api', apiRoutes)
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
+
+const url = `https://test1-swx6.onrender.com`
+const interval = 30000
+
+function reloadWebsite() {
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        console.log('Pinged backend successfully!!')
+      } else {
+        console.log('Failed to ping backend.')
+      }
+    })
+    .catch((error) => {
+      console.log("Can't ping backend!", error)
+    })
+}
+
+setInterval(reloadWebsite, interval)
